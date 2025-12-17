5c6aa30a6a74121ff27b635842caf0d132c68d97
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-18 01:47:43 +0530

    [Akshay]:added new file to kilometer to miles

---
+++ gcr-codebase/programming-elements/level-1/KilometerToMiles.java
@@ -0,0 +1,18 @@
+// Create KilometerToMiles Class to covert distance from kilometer to miles
+public class KilometerToMiles{
+   public static void main(String args[]){ 
+
+      // declare and initialize the distance in kilometers
+      double kilometer = 10.8;   
+      double miles = kilometer * 1.6;
+       
+      // Display the result on the screen
+      System.out.println("The distance "+kilometer+" km in miles is "+miles);   
+   }
+} 
+
+
+
+
+
+
