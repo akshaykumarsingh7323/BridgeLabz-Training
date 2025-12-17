25c8988739ee3419c90cbcebd17baf9552431830
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-17 17:30:31 +0530

    [Akshay] : added level-1 folder and add HurryAge program

---
+++ gcr-codebase/programming-elements/level-1/HarryAge.java
@@ -0,0 +1,19 @@
+// Create HarryAge Class to calculate age of Harry
+public class HarryAge{
+   public static void main(String args[]){ 
+
+      // Create int variable birthyear and currentYear and assign value 2000 and 2024
+      int birthYear=2000;
+      int currentYear=2024;
+      int age = currentYear - birthYear;
+
+      // Display result
+      System.out.println("Harry's age in 2024 is "+age);   
+   }
+} 
+
+
+
+
+
+
