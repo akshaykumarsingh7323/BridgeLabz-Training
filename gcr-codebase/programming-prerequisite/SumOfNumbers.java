dc485eba82d1ccead7d391bc7aa1962029490f6c
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-17 16:22:27 +0530

    [Akshay] : addded a program of sum of number

---
+++ gcr-codebase/programming-prerequisite/SumOfNumbers.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+ class SumOfNumbers{
+   public static void main(String args[]){
+	Scanner kb=new Scanner(System.in);	
+        System.out.println("Enter first number:");
+	int a = kb.nextInt();
+	System.out.println("Enter Second number:");
+	int b = kb.nextInt();
+ 	System.out.println("Sum of two number is:"+(a+b));
+   }
+} 
+
+
+
+
+
+
+
+
+
+
+
