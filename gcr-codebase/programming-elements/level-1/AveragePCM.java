819ebccfebfe0ee7817350ceb3700ed7b9fcd6b8
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-18 01:44:50 +0530

    [Akshay]:Added a program to find average of three subject

---
+++ gcr-codebase/programming-elements/level-1/AveragePCM.java
@@ -0,0 +1,21 @@
+// Create AveragePCM Class to find the average percentage marks in PCM
+public class AveragePCM{
+   public static void main(String args[]){ 
+
+      // declare and initialize marks for each subject
+      int maths = 94;
+      int physics = 95;
+      int chemistry = 96;
+      int total = maths + physics + chemistry;   
+      double avg = total/3.0;
+       
+      // Display the result on the screen
+      System.out.println("Sam's average marks in PCM is "+avg);   
+   }
+} 
+
+
+
+
+
+
