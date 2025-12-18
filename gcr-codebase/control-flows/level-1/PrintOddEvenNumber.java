8599af5dda9c788ef1c67edf9b498b416c159a4b
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-19 04:16:11 +0530

    [Akshay]:added a program to print odd and even number from 1 to given number

---
+++ gcr-codebase/control-flows/level-1/PrintOddEvenNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+// create a class to print odd and even number 
+
+public class PrintOddEvenNumber{
+    public static void main(String args []){
+
+	//Get input from the user and checking odd or even and displaying the output
+
+	Scanner kb = new Scanner(System.in);
+
+	System.out.println("Enter a Number:");
+	int number = kb.nextInt();
+	
+	if(number <= 0){
+
+	   System.out.println("Please enter a natural number!"); 
+ 
+	}else{
+           
+	   for(int i = 1 ; i <= number ; i++){
+
+	       if(i % 2 == 0){
+
+		  System.out.println(i + " is an even number");
+
+	       }else{
+
+                  System.out.println(i + " is an odd number");
+	       }	
+	   }
+	}
+    }
+}
+
