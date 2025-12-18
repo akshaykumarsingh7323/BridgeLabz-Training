c1fe5a487245474b762aa7c1f9bcfd3a8607fabd
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-18 17:08:04 +0530

    [Akshay]:added a folder level-1 with a file CheckDivisibility

---
+++ gcr-codebase/control-flows/level-1/CheckDivisibility.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// create a class to check divisibility of a number
+public class CheckDivisibility{
+    public static void main(String args []){
+
+	// Declare a variable and assign value by taking from users
+	Scanner kb = new Scanner(System.in);
+
+	System.out.println("Enter a number:");
+	int number = kb.nextInt();
+
+	// Displaying the output "true" or "false"
+        System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));			  
+     }
+}
+
+
