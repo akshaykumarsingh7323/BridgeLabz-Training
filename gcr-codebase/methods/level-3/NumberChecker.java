380657abee83b6156d9bf32e24e6ffa93f2fa741
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-22 15:27:20 +0530

    [Akshay]: added a program to check number

---
+++ gcr-codebase/methods/level-3/NumberChecker.java
@@ -0,0 +1,99 @@
+public class NumberChecker {
+
+    public static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    public static int[] getDigitsArray(int number) {
+        int count = countDigits(number);
+        int[] digits = new int[count];
+
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    public static boolean isDuckNumber(int[] digits) {
+        for (int d : digits) {
+            if (d != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isArmstrong(int number, int[] digits) {
+        int power = digits.length;
+        int sum = 0;
+
+        for (int d : digits) {
+            sum += Math.pow(d, power);
+        }
+        return sum == number;
+    }
+
+    public static void findLargestAndSecondLargest(int[] digits) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int d : digits) {
+            if (d > largest) {
+                secondLargest = largest;
+                largest = d;
+            } else if (d > secondLargest && d != largest) {
+                secondLargest = d;
+            }
+        }
+
+        System.out.println("Largest Digit: " + largest);
+        System.out.println("Second Largest Digit: " + secondLargest);
+    }
+
+    public static void findSmallestAndSecondSmallest(int[] digits) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int d : digits) {
+            if (d < smallest) {
+                secondSmallest = smallest;
+                smallest = d;
+            } else if (d < secondSmallest && d != smallest) {
+                secondSmallest = d;
+            }
+        }
+
+        System.out.println("Smallest Digit: " + smallest);
+        System.out.println("Second Smallest Digit: " + secondSmallest);
+    }
+
+    public static void main(String[] args) {
+
+        int number = 153;
+
+        System.out.println("Number: " + number);
+
+        int digitCount = countDigits(number);
+        System.out.println("Count of Digits: " + digitCount);
+
+        int[] digits = getDigitsArray(number);
+
+        System.out.print("Digits Array: ");
+        for (int d : digits) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+
+        System.out.println("Is Duck Number: " + isDuckNumber(digits));
+        System.out.println("Is Armstrong Number: " + isArmstrong(number, digits));
+
+        findLargestAndSecondLargest(digits);
+        findSmallestAndSecondSmallest(digits);
+    }
+}
