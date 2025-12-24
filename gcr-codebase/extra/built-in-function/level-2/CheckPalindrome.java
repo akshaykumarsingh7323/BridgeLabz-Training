379842bbdd88f1bffff957be326d98936d956eb0
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-24 13:07:43 +0530

    [Akshay]: added a program to check palindrome

---
+++ gcr-codebase/extra/built-in-function/level-2/CheckPalindrome.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class CheckPalindrome{
+
+    private static String takeInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        return sc.nextLine();
+    }
+
+    private static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    private static void displayResult(String str, boolean result) {
+        if(result) {
+            System.out.println("\"" + str + "\" is a PALINDROME");
+        } else {
+            System.out.println("\"" + str + "\" is NOT a palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = takeInput();
+        boolean result = isPalindrome(input);
+        displayResult(input, result);
+    }
+}
