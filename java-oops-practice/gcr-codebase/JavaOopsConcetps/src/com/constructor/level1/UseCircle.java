601bf1d069788639a8d3147c6414d1f6d486d535
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2025-12-29 01:37:34 +0530

    [Akshay]: added a program to show circle details

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcetps/src/com/constructor/level1/UseCircle.java
@@ -0,0 +1,12 @@
+package com.constructor.level1;
+
+public class UseCircle {
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+		c1.display();
+		Circle c2 = new Circle(5.5);
+		c2.display();
+	}
+
+}
