91dfb4c57a35d39ffa78cc722bc3c907411693ed
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-02 21:04:20 +0530

    [Akshay] : added a program of E-Wallet application

---
+++ java-oops-practice/scenario-based-codebase/ScenarioPractice/src/com/ewalletapplication/User.java
@@ -0,0 +1,15 @@
+package com.ewalletapplication;
+
+public class User {
+
+    private String name;
+    private String userId;
+
+    public User(String name, String userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getName() { return name; }
+    public String getUserId() { return userId; }
+}
