fbcce1639b9bb1c69e5ada5149cdba8cda70e2a4
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-05 16:34:09 +0530

    [Akshay] : added a program of Sliding Window Maximum

---
+++ dsa-practice/gcr-codebase/DsaPractice/src/com/stacksandqueues/slidingwindowmaximum/SlidingWindowMaximum.java
@@ -0,0 +1,60 @@
+package com.stacksandqueues.slidingwindowmaximum;
+
+import java.util.*;
+
+class SlidingWindowMaximum {
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0)
+            return new int[0];
+
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter window size k: ");
+        int k = sc.nextInt();
+
+        int[] result = maxSlidingWindow(arr, k);
+
+        System.out.println("Sliding Window Maximums:");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+    }
+}
