a356e32581f25937cdb00e59c4a82772c2bc4dbb
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-07 15:09:20 +0530

    [Akshay] : added a program to find first negative number in the array

---
+++ dsa-practice/gcr-codebase/DsaPractice/src/com/linearandbinarysearch/SearchFirstNegative.java
@@ -0,0 +1,37 @@
+package com.linearandbinarysearch;
+
+import java.util.Scanner;
+
+public class SearchFirstNegative {
+	
+	static int checkNumber(int arr []) {
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] < 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String args []) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter size of Array: ");
+		int n = sc.nextInt();
+		
+		int arr [] = new int [n];
+		System.out.println("Enter " + n + "  numbers: ");
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int result = checkNumber(arr);
+		
+		if(result < 0) {
+			System.out.println("-1");
+		}else {
+			System.out.println("Number on index " + result + " is Negative");
+		}
+	}
+}
