b79a2015c467a97d70fd1a058bfd65aede27fb21
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-09 17:25:25 +0530

    [Akshay] : added a program to find missing number and index in the array

--- dsa-practice/gcr-codebase/DsaPractice/src/com/linearandbinarysearch/LinearAndBinarySearch.java
+++ dsa-practice/gcr-codebase/DsaPractice/src/com/linearandbinarysearch/LinearAndBinarySearch.java
@@ -2,35 +2,35 @@ package com.linearandbinarysearch;
 
 import java.util.*;
 
-public class FindMissingNumberUsingLinearSearch {
+public class LinearAndBinarySearch {
 
-    public static int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] present = new boolean[n + 1];
+    public static int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        boolean[] visited = new boolean[n + 1];
 
-        for (int num : nums) {
+        for (int num : arr) {
             if (num > 0 && num <= n) {
-                present[num] = true;
+                visited[num] = true;
             }
         }
 
         for (int i = 1; i <= n; i++) {
-            if (!present[i]) {
+            if (!visited[i]) {
                 return i;
             }
         }
         return n + 1;
     }
 
-    public static int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
 
         while (left <= right) {
             int mid = (left + right) / 2;
 
-            if (nums[mid] == target)
+            if (arr[mid] == target)
                 return mid;
-            else if (nums[mid] < target)
+            else if (arr[mid] < target)
                 left = mid + 1;
             else
                 right = mid - 1;
@@ -45,8 +45,9 @@ public class FindMissingNumberUsingLinearSearch {
         int missing = firstMissingPositive(arr);
         System.out.println("First Missing Positive: " + missing);
 
-        Arrays.sort(arr); 
+        Arrays.sort(arr);
         int index = binarySearch(arr, target);
         System.out.println("Index of target " + target + ": " + index);
     }
 }
+
