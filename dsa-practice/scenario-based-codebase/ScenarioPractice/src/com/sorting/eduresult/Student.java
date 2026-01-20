8a0850c9a4b0b61dd50582daa8a12c7aa2fac1c8
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-20 14:44:47 +0530

    [Akshay] : added a program to sort student marks

---
+++ dsa-practice/scenario-based-codebase/ScenarioPractice/src/com/sorting/eduresult/Student.java
@@ -0,0 +1,11 @@
+package com.sorting.eduresult;
+
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
---
+++ dsa-practice/scenario-based-codebase/ScenarioPractice/src/com/sorting/eduresult/UseEduResult.java
@@ -0,0 +1,34 @@
+package com.sorting.eduresult;
+
+import java.util.Scanner;
+
+public class UseEduResult {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine(); 
+
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter student name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter marks: ");
+            int marks = sc.nextInt();
+            sc.nextLine();
+
+            students[i] = new Student(name, marks);
+        }
+
+        MergeSort.mergeSort(students, 0, n - 1);
+
+        System.out.println("\nState-wise Rank List:");
+        int rank = 1;
+        for (Student s : students) {
+            System.out.println("Rank " + rank++ + ": " + s.name + " - " + s.marks);
+        }
+    }
+}
