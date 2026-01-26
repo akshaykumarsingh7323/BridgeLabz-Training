f8da9982f35b0842fc0a83bce08bb813f4ba2d33
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-26 15:57:05 +0530

    [Akshay] : added a program for testing password strength

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/advanceproblem/testingpasswordstrength/PasswordValidatorTest.java
@@ -0,0 +1,40 @@
+package com.junit.advanceproblem.testingpasswordstrength;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+
+    PasswordValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new PasswordValidator();
+    }
+
+    @Test
+    void testValidPassword() {
+        assertTrue(validator.isValid("Password1"));
+    }
+
+    @Test
+    void testShortPassword() {
+        assertFalse(validator.isValid("Pass1"));
+    }
+
+    @Test
+    void testNoUppercase() {
+        assertFalse(validator.isValid("password1"));
+    }
+
+    @Test
+    void testNoDigit() {
+        assertFalse(validator.isValid("Password"));
+    }
+
+    @Test
+    void testNullPassword() {
+        assertFalse(validator.isValid(null));
+    }
+}
