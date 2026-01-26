6f3146fb1aed21546a0eeb2fd4eb58a4f8da6cbe
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-26 12:18:26 +0530

    [Akshay] : added a program to validate social security number

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/regex/advanceproblem/ValidateSSN.java
@@ -0,0 +1,22 @@
+package com.regex.advanceproblem;
+
+// SSN - SOcial Security Number
+import java.util.regex.*;
+
+public class ValidateSSN {
+    public static void main(String[] args) {
+
+        String text = "My SSN is 123-45-6789.";
+
+        String regex = "\\b\\d{3}-\\d{2}-\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            System.out.println("✅ \"" + matcher.group() + "\" is valid");
+        } else {
+            System.out.println("❌ No valid SSN found");
+        }
+    }
+}
+
