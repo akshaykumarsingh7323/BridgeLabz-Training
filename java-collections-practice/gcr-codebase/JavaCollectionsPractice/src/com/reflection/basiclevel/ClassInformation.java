8b3fbac4480089ca4f48e36d211d51d412ccea1f
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-27 09:49:54 +0530

    [Akshay] : added a program to find class information

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/basiclevel/ClassInformation.java
@@ -0,0 +1,46 @@
+package com.reflection.basiclevel;
+
+import java.lang.reflect.*;
+import java.util.Scanner;
+
+public class ClassInformation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter class name: ");
+        String className = sc.nextLine();
+
+        try {
+            // load class
+            Class<?> cls = Class.forName(className);
+
+            System.out.println("\nClass Name: " + cls.getName());
+
+            // methods
+            System.out.println("\nMethods:");
+            Method[] methods = cls.getDeclaredMethods();
+            for (Method m : methods) {
+                System.out.println(m.getName());
+            }
+
+            // fields
+            System.out.println("\nFields:");
+            Field[] fields = cls.getDeclaredFields();
+            for (Field f : fields) {
+                System.out.println(f.getName());
+            }
+
+            // constructor
+            System.out.println("\nConstructors:");
+            Constructor<?>[] constructors = cls.getDeclaredConstructors();
+            for (Constructor<?> c : constructors) {
+                System.out.println(c);
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found!");
+        }
+    }
+}
