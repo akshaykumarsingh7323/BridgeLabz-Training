a542de6672809fb37b4b13dae59e617a24c19a59
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-01-28 01:19:41 +0530

    [Akshay] : added a program to use @Override correctly

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/basicannotations/OverrideExample.java
@@ -0,0 +1,25 @@
+package com.annotations.basicannotations;
+
+class Animal {
+
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class OverrideExample {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
