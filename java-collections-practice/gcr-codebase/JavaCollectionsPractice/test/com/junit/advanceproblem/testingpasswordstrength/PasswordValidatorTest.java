86b6fc3380d1d8493f6bfb0fcbc861f5577a7b9e
Akshay Kumar Singh <akshaymaapapa123@gmail.com>
2026-02-04 16:11:33 +0530

    [Akshay] : added a program for testing password strength

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/test/com/junit/advanceproblem/testingpasswordstrength/PasswordValidatorTest.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/test/com/junit/advanceproblem/testingpasswordstrength/PasswordValidatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.junit.advanceproblem.testingpasswordstrength.PasswordValidator;
+
 public class PasswordValidatorTest {
 
     PasswordValidator validator;
